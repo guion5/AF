873ed43ba361437fea806f276b781fd65fb7408a
guion5 <127799018+guion5@users.noreply.github.com>
2023-06-09 16:10:09 -0300

    Update Calculadora_Funcao.cs

--- Calculadora_Funcao.cs
+++ Calculadora_Funcao.cs
@@ -1,58 +1,40 @@
 using System;
-using System.Globalization;
 
 public class Program
 {
     public static float Soma(float a, float b)
     {
-        float Soma = a + b;
-        //resultado do calculo das variáveis (a,b)
-        return Soma;
+        float resultado1;
+        resultado1 = a + b;
+        return resultado1;
     }
 
-    //declaração de variávels
     public static float Subt(float a, float b)
     {
-        float Subt = a - b;
-        //resultado do calculo das variáveis (a,b)
-        return Subt;
+        float resultado2;
+        resultado2 = a - b;
+        return resultado2;
     }
 
-    //declaração de variávels
     public static float Mult(float a, float b)
     {
-        float Mult = a * b;
-        //resultado do calculo das variáveis (a,b)
-        return Mult;
+        float resultado3;
+        resultado3 = a * b;
+        return resultado3;
     }
 
-    //declaração de variávels
     public static float Divi(float a, float b)
     {
-        float Divi = a / b;
-        //resultado do calculo das variáveis (a,b)
-        return Divi;
+        float resultado4;
+        resultado4 = a / b;
+        return resultado4;
     }
-    //abertura do código
-    static void Main(string[] args)
-    {
-        float soma = Soma(3, 5);
-        float subt = Subt(3, 5);
-        float mult = Mult(3, 5);
-        float divi = Divi(3, 5);
 
-        {
-            //interface do usuário com o resultado do primeiro calculo (valores fixos)
-            Console.WriteLine(soma.ToString(CultureInfo.InvariantCulture));
-            //interface do usuário com o resultado do segundo calculo (valores fixos)
-            Console.WriteLine(subt.ToString(CultureInfo.InvariantCulture));
-            //interface do usuário com o resultado do terceiro calculo (valores fixos)
-            Console.WriteLine(mult.ToString(CultureInfo.InvariantCulture));
-            //interface do usuário com o resultado do quarto calculo (valores fixos)
-            Console.WriteLine(divi.ToString(CultureInfo.InvariantCulture));
-        }
+    public static void Main()
+    {
+        Console.WriteLine(Soma(3, 5));
+        Console.WriteLine(Subt(3, 5));
+        Console.WriteLine(Mult(3, 5));
+        Console.WriteLine(Divi(3, 5));
     }
 }
-
-
-
