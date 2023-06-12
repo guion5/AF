bfd7f36af2b66313121006ac0a42c8436d2bb7d7
guion5 <127799018+guion5@users.noreply.github.com>
2023-06-12 19:29:36 -0300

    Update DoWhile.cs

--- DoWhile.cs
+++ DoWhile.cs
@@ -1,17 +1,25 @@
+//inclusão de biblioteca
 using System;
 
+//classe do programa
 public class Program
 {
+    //programa principal - abertura do código
     public static void Main()
     {
+        //declaração de variável
         int num;
         
+        //ação inicial
         do
         {
+            //interface do usuário para inserir valor
             Console.Write("Digite um numero entre 1 e 10: ");
             num = int.Parse(Console.ReadLine());
+          //condicional para valores menores que 1 ou maiores que 10  
         } while (num < 1 || num > 10);
 
+        //interface do usuário com a resposta
         Console.WriteLine("Você digitou o número " + num + ".");
     }
 }
