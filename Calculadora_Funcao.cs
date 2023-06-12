1425fe7aad13ee6a310cdc58ca96f003fdca960d
guion5 <127799018+guion5@users.noreply.github.com>
2023-06-12 19:26:15 -0300

    Update Calculadora_Funcao.cs

--- Calculadora_Funcao.cs
+++ Calculadora_Funcao.cs
@@ -1,37 +1,49 @@
+//inclusão de biblioteca
 using System;
 
+//classe do programa
 public class Program
 {
+    //declaração de variável
     public static float Soma(float a, float b)
     {
         float resultado1;
+        //cálculo 1
         resultado1 = a + b;
         return resultado1;
     }
 
+    //declaração de variável
     public static float Subt(float a, float b)
     {
         float resultado2;
+        //cálculo 2
         resultado2 = a - b;
         return resultado2;
     }
 
+    //declaração de variável
     public static float Mult(float a, float b)
     {
         float resultado3;
+        //cálculo 3
         resultado3 = a * b;
         return resultado3;
     }
 
+    //declaração de variável
     public static float Divi(float a, float b)
     {
         float resultado4;
+        //cálculo 4
         resultado4 = a / b;
         return resultado4;
     }
 
+    //programa principal
     public static void Main()
     {
+        //interface do usuário
         Console.WriteLine(Soma(3, 5));
         Console.WriteLine(Subt(3, 5));
         Console.WriteLine(Mult(3, 5));
