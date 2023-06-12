90909c4dc354ecb8de1729c0c7defd76bcb735cc
guion5 <127799018+guion5@users.noreply.github.com>
2023-06-12 19:37:18 -0300

    Update While.cs

--- While.cs
+++ While.cs
@@ -1,15 +1,22 @@
+//inclusão de bibliotecas
 using System;
 
+//classe do programa
 public class Program
 {
+    //abertura do código principal
     public static void Main()
     {
+        //declaração de variável
         int num, i = 1;
+        //interface do usuário
         Console.Write("Digite um numero inteiro positivo: ");
         num = int.Parse(Console.ReadLine());
         
+        //ação do condicional
         while (i <= num)
         {
+            //interface do usuário com a contagem
             Console.Write(i + " ");
             i++;
         }
