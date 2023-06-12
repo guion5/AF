1cef52f9fcce7e33be63013dfb6d28c543c26b41
guion5 <127799018+guion5@users.noreply.github.com>
2023-06-12 19:33:00 -0300

    Update For.cs

--- For.cs
+++ For.cs
@@ -1,18 +1,26 @@
+//inclusão de bibliotecas
 using System;
 
+//classe do programa
 public class Program
 {
+    //abertura do código principal
     public static void Main()
     {
+        //declaraçãod e variável
         int num;
 
+        //interface do usuário
         Console.Write("Digite um numero inteiro positivo: ");
         num = int.Parse(Console.ReadLine());
 
+        //interface do usuário
         Console.Write("Os primeiros " + num + " numeros pares sao: ");
         
+        //ação da condicional
         for (int i = 2; i <= num * 2; i += 2)
         {
+            //interface do usuário
             Console.Write(i + " ");
         }
 
