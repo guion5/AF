8e3ce1e65e27d97819c3349ee43164e91355abaa
guion5 <127799018+guion5@users.noreply.github.com>
2023-06-12 19:20:26 -0300

    Update Calculadora_Switch.cs

--- Calculadora_Switch.cs
+++ Calculadora_Switch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+//incluir biblioteca
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace DecisionMaking3
 {
+    //classe do programa
     class Program
     {
+        //abertura de código
         static void Main(string[] args)
         {
             Console.WriteLine("Calculadora");
@@ -17,46 +20,62 @@ namespace DecisionMaking3
             Console.WriteLine("3.Multiplicação");
             Console.WriteLine("4.Divisão");
 
+            //Interface de usuário
             Console.Write("Escolha uma operação (1-4):");
             int ch = Int32.Parse(Console.ReadLine());
 
+            //declaração de variáveis
             int a, b, c;
 
+            //condicionais switch e cases
             switch (ch)
             {
                 case 1:
+                    //Interface de usuário
                     Console.Write("Insira o primeiro valor:");
                     a = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Insira o segundo valor:");
                     b = Convert.ToInt32(Console.ReadLine());
+                    //cálculo com os valores atribuidos as variáveis
                     c = a + b;
+                    //Interface de usuário com o resultado do cálculo
                     Console.WriteLine("Resultado = {0}", c);
                     break;
                 case 2:
+                    //Interface de usuário
                     Console.Write("Insira o primeiro valor:");
                     a = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Insira o segundo valor:");
                     b = Convert.ToInt32(Console.ReadLine());
+                    //cálculo com os valores atribuidos as variáveis
                     c = a - b;
+                    //Interface de usuário com o resultado do cálculo
                     Console.WriteLine("Resultado = {0}", c);
                     break;
                 case 3:
+                    //Interface de usuário
                     Console.Write("Insira o primeiro valor:");
                     a = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Insira o segundo valor:");
                     b = Convert.ToInt32(Console.ReadLine());
+                    //cálculo com os valores atribuidos as variáveis
                     c = a * b;
+                    //Interface de usuário com o resultado do cálculo
                     Console.WriteLine("Resultado = {0}", c);
                     break;
                 case 4:
+                    //Interface de usuário
                     Console.Write("Insira o primeiro valor:");
                     a = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Insira o segundo valor:");
                     b = Convert.ToInt32(Console.ReadLine());
+                     //cálculo com os valores atribuidos as variáveis
                     c = a / b;
+                    //Interface de usuário com o resultado do cálculo
                     Console.WriteLine("Resultado = {0}", c);
                     break;
                 default:
+                    //Interface de usuário
                     Console.WriteLine("Escolha inválida");
                     break;
             }
